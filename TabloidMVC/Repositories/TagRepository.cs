3dfb98d1d2a47b8f822df7069668d967e016a083
dparsley1203 <dparsley1203@gmail.com>
2021-09-28 10:13:44 -0500

    added Tag repo and model

---
+++ TabloidMVC/Repositories/TagRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Reflection.PortableExecutable;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using TabloidMVC.Models;
+using TabloidMVC.Utils;
+
+
+namespace TabloidMVC.Repositories
+{
+    public class TagRepository : BaseRepository, ITagRepository
+    {
+        public TagRepository(IConfiguration config) : base(config) { }
+        public List<Tag> GetAllTags()
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT Id, Name
+                                        FROM Tag";
+
+                    var reader = cmd.ExecuteReader();
+                    var tags = new List<Tag>();
+
+                    while (reader.Read())
+                    {
+                        var tag = new Tag()
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Name = reader.GetString(reader.GetOrdinal("Name")),
+                        };
+                    }
+                    reader.Close();
+                    return tags;
+                }
+            }
+        }
+    }
+}
